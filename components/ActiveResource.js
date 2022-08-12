dd22fc589a09f2dfdf7fca2a6164a0c250880132
Daniel Silverio <daniel.silverio@mariachi.io>
2022-08-12 16:46:10 -0500

    Se agrego la seccion de recursos activos dentro de la pagina web

---
+++ components/ActiveResource.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Link from "next/link";
+
+export default function ActiveResource() {
+  return (
+    <div className="active-resource">
+      <h1 className="resource-name">My Active Resource</h1>
+      <div className="time-wrapper">
+        <h2 className="elapsed-time">1400</h2>
+      </div>
+      <Link href="/">
+        <a className="button">Go tu resource</a>
+      </Link>
+    </div>
+  );
+}
--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "components/Navbar";
 import Footer from "components/Footer";
+import ActiveReource from "components/ActiveResource"
 
 export default function Layout({ children }) {
   return (
     <>
       <Navbar />
+      <ActiveReource />
       {children}
       <Footer />
     </>
